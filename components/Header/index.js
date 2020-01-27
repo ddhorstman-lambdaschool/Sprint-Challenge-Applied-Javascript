f2e06c7efee2fe8abef29d82882d78f125a2e3ce
ddhorstman <davidzilla27@gmail.com>
2020-01-26 21:17:52 -0500

    Header.

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,30 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+Header();
 
-function Header() {}
+function Header() {
+    const months = ['January', 'February','March'];
+    let now = new Date(Date.now());
+    let dateString = `${months[now.getMonth()]} ${now.getDate()}, ${now.getFullYear()}`;
+    const header = makeHeader(dateString);
+    document.querySelector('.header-container').appendChild(header);
+}
+
+function makeHeader(_date){
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    header.classList.add('header');
+    date.classList.add('date');
+    date.textContent=_date;
+    title.textContent="Lambda Times";
+    temp.classList.add('temp');
+    temp.textContent ="41°";
+
+    header.append(date,title,temp);
+
+    return header;
+}
